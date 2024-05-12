a6705ba8c444200e1f9242deedacb0bae3cae7c5
emmachizo123 <emmanuelchizobaokonkwo@gmail.com>
2024-05-12 16:31:06 +0100

    CSV funtionality
    1. Added a 'write_to_csv' function to the resource_utility.py that writes the pandas  Dataframe for each resource to a csv file.
    
    2. The function creates a CSVFolder in the project folder if it doesn't exist and saves the csv in the folder
    
    Tested it out and its working

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pathlib
 from resource_utility import create_resource_list
 from dataframe_converters import *
 
-
 def main() -> None:
     """
         Function processes resource type medical information stored
@@ -59,6 +58,60 @@ def main() -> None:
     claim_df = fhir_claim_to_dataframe(resDict['Claim'])
     print("****** Claim DataFrame*****")
     print(claim_df.head())
+
+
+if __name__ == '__main__':
+    main()
+
+import json
+import pathlib
+from resource_utility import create_resource_list, write_to_csv
+from dataframe_converters import *
+
+
+def main() -> None:
+    """
+    Function processes resource type medical information stored
+    in a json file and converts them to dataframes and stores them
+    in a csv file
+
+    Returns
+    -------
+    None
+
+    """
+
+    """Initially had the json file in my drive. I copied it over to the project directory"""
+    # file_path = pathlib.Path(r'C:\Emis\rhir.json')
+
+    # with open(file_path, 'r') as f:
+    # fhir_data = json.load(f)
+
+    with open("rhir.json", 'r') as f:
+        fhir_data = json.load(f)
+
+    resType = []
+    for rType in fhir_data['entry']:
+        resType.append(rType['resource']['resourceType'])
+
+    resDict = {key: [] for key in set(resType)}
+    create_resource_list(fhir_data['entry'], resDict)
+
+    patient_df = fhir_patient_to_dataframe(resDict['Patient'])
+    condition_df = fhir_condition_to_dataframe(resDict['Condition'])
+    diagnostic_df = fhir_diagnosticReport_to_dataframe(resDict['DiagnosticReport'])
+    observaton_df = fhir_observation_to_dataframe(resDict['Observation'])
+    procedure_df = fhir_procedure_to_dataframe(resDict['Procedure'])
+    claim_df = fhir_claim_to_dataframe(resDict['Claim'])
+
+    write_to_csv(
+        ('patient', patient_df),
+        ('condition', condition_df),
+        ('diagnostic', diagnostic_df),
+        ('observation', observaton_df),
+        ('procedure', procedure_df),
+        ('claim', claim_df)
+    )
 
 
 if __name__ == '__main__':
--- resource_utility.py
+++ resource_utility.py
@@ -1,7 +1,4 @@
-
-'''
-This file contains
-'''
+import os
 
 from fhir.resources.claim import Claim
 from fhir.resources.condition import Condition
@@ -9,49 +6,104 @@ from fhir.resources.diagnosticreport import DiagnosticReport
 from fhir.resources.observation import Observation
 from fhir.resources.patient import Patient
 from fhir.resources.procedure import Procedure
+from fhir.resources import FHIRAbstractModel
+from typing import List, Type
 
 
-def create_resource_list(list_of_resource, resource_dict):
+def create_resource_list(list_of_resource: List, resource_dict: dict) -> None:
     """
-        The function takes the entry from a bundle, initializes the data to the right
-        resource and groups the different resource data into the resource dictionary.
-        Mutates the resource_dict inplace
+    The function takes the entry from a bundle, initializes the data to the right
+    resource and groups the different resource data into the resource dictionary.
+    Mutates the resource_dict inplace
 
-        Parameters
-        ----------
-        list_of_resource : List
-            This is the entry from a bundle, and is a list of several resources.
-        resource_dict : dict
-            (key = name_of_resource, value = list of resource data ). data is processed into the dictionary
+    Parameters
+    ----------
+    list_of_resource : List
+        This is the entry from a bundle, and is a list of several resources.
+    resource_dict : dict
+        (key = name_of_resource, value = list of resource data ). data is processed into the dictionary
 
-        Returns
-        -------
-        None
+    Returns
+    -------
+    None
 
-        """
-
+    """
     for res in list_of_resource:
-        field_dict = {}
+        field_dict = {}  # will contain the attributes (and their values) that belong to a resource
+
         if res['resource']['resourceType'] == 'Patient':
-            filter_field(Patient, res['resource'], field_dict)
+            _filter_field(Patient, res['resource'], field_dict)
             resource_dict['Patient'].append(Patient(**field_dict))
         elif res['resource']['resourceType'] == 'Condition':
-            filter_field(Condition, res['resource'], field_dict)
+            _filter_field(Condition, res['resource'], field_dict)
             resource_dict['Condition'].append(Condition(**field_dict))
         elif res['resource']['resourceType'] == 'DiagnosticReport':
-            filter_field(DiagnosticReport, res['resource'], field_dict)
+            _filter_field(DiagnosticReport, res['resource'], field_dict)
             resource_dict['DiagnosticReport'].append(DiagnosticReport(**field_dict))
         elif res['resource']['resourceType'] == 'Observation':
-            filter_field(Observation, res['resource'], field_dict)
+            _filter_field(Observation, res['resource'], field_dict)
             resource_dict['Observation'].append(Observation(**field_dict))
         elif res['resource']['resourceType'] == 'Procedure':
-            filter_field(Procedure, res['resource'], field_dict)
+            _filter_field(Procedure, res['resource'], field_dict)
             resource_dict['Procedure'].append(Procedure(**field_dict))
         elif res['resource']['resourceType'] == 'Claim':
-            filter_field(Claim, res['resource'], field_dict)
+            _filter_field(Claim, res['resource'], field_dict)
             resource_dict['Claim'].append(Claim(**field_dict))
 
-def filter_field(model, entry, field_dict):
+def _filter_field(model: FHIRAbstractModel, entry: dict, field_dict: dict) -> None:
+    """
+    Filters off attributes that do not belong to the resource type, and stores
+    the right attributes and its values in the dictionary - field_dict
+
+    Parameters
+    ----------
+    model : FHIRAbstractModel
+        The resource type .
+    entry : dict
+        A single resource data of the passed resource type.
+    field_dict : dict
+        Dictionary to store the right attributes and its value.
+
+    Returns
+    -------
+    None
+
+    """
     for key in entry.keys():
         if key in model.elements_sequence():
             field_dict[key] = entry[key]
+
+def write_to_csv(*args, file_path : str = '.' )-> None:
+    """
+
+
+    Parameters
+    ----------
+    *args : a List of tuple - ( name_of_file, dataframe)
+
+    file_path : str, optional
+        Directory path to store the data. The default is '.'.
+
+    Returns
+    -------
+    None
+
+    """
+
+    # Get the current working directory
+    current_directory = os.getcwd()
+
+    #Define the folder name for the csv files
+    folder_name ='CSVFolder'
+
+    #Create the folder if it doesn't exist
+    folder_path = os.path.join(current_directory,folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+
+    #for file_name, df in args:
+        #df.to_csv(f'{file_path}/{file_name}.csv', index=False)
+
+    for file_name, df in args:
+        df.to_csv(f'{folder_path}/{file_name}.csv', index=False)
